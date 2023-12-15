5eaa67ef3f72c0a68e6832be5917747fc92aadb0
Liam Seys <liam.seys@gmail.com>
2023-12-15 13:54:30 +0100

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -94,8 +94,8 @@ function ContactDetails() {
         </h2>
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
           {[
-            ['General', 'info@adframe.com'],
-            ['Press', 'press@adframe.com'],
+            ['General Inquiries', 'info@adframe.com'],
+            ['Copyright & Licensing', 'copyright@adframe.com'],
           ].map(([label, email]) => (
             <div key={email}>
               <dt className="font-semibold text-neutral-950">{label}</dt>
