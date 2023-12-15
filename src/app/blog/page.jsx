7604bac905d5758ba4cc072f4e03c9a08f467707
Liam Seys <liam.seys@gmail.com>
2023-12-15 11:20:28 +0100

    Update page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -13,7 +13,7 @@ import { loadMDXMetadata } from '@/lib/loadMDXMetadata'
 export const metadata = {
   title: 'Blog',
   description:
-    'Stay up-to-date with the latest industry news as our marketing teams finds new ways to re-purpose old CSS tricks articles.',
+    'Get the latest media industry news on our blog, brought to you by our expert marketing team.',
 }
 
 export default async function Blog() {
@@ -23,8 +23,7 @@ export default async function Blog() {
     <>
       <PageIntro eyebrow="Blog" title="The latest articles and news">
         <p>
-          Stay up-to-date with the latest industry news as our marketing teams
-          finds new ways to re-purpose old CSS tricks articles.
+            Get the latest media industry news on our blog, brought to you by our expert marketing team.
         </p>
       </PageIntro>
 
